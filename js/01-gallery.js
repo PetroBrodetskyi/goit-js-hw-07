16d35a0734315a36d066b9e2147e716ee874e65e
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2023-08-31 16:35:20 +0200

    Connecting the library and the first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryElem = galleryItems
+    .map(({ preview, description, original }) => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </li>`)
+    .join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryElem)
+
+galleryContainer.addEventListener('click', onImgClick)
+
+function onImgClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const modal = basicLightbox.create(
+        `<img src="${evt.target.dataset.source}" width="800" height="600">`,
+
+        {   onShow: () => window.addEventListener('keydown', onEscKeyPress),
+            onClose: () => window.removeEventListener('keydown', onEscKeyPress),
+        }
+    );
+    
+    modal.show();
+
+    function onEscKeyPress(evt) {   
+        if (evt.code === "Escape") {
+            modal.close();
+        }
+    }
+}
+
+
+
+
+
+
